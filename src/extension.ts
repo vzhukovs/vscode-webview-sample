8db0012ae2e96959ba52fa11943cdc619deb1b1f
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2020-12-16 00:28:06 +0200

    che#18405
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,52 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
-
-export const schemeID = 'file-sidecar-memfs';
-export const scheme = schemeID + ':/';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Setup File System Provider
-	const memFs = new MemFS();
-	context.subscriptions.push(vscode.workspace.registerFileSystemProvider(schemeID, memFs, { isCaseSensitive: true }));
+	const sidecarName = process.env.CHE_MACHINE_NAME;
+    if (sidecarName == undefined) {
+	  console.log('Side Car name is undefined.');
+	  return;
+	}
+	
+	const sidecarScheme = `file-sidecar-${sidecarName}`;
+	console.log(`Side Car scheme: ${sidecarScheme}`);
+	console.log(`Content extension path: ${context.extensionPath}`);
 
-	memFs.writeFile(vscode.Uri.parse(`${scheme}styles.css`), Buffer.from(styleContent), { create: true, overwrite: true });
-
-	// Setup Webview
-	context.subscriptions.push(vscode.commands.registerCommand('memfs.showWebview', _ => {
-		MemFSWebviewPanel.show(context.extensionPath);
+	context.subscriptions.push(vscode.commands.registerCommand('sidecar.showWebview', _ => {
+		MemFSWebviewPanel.show(context.extensionPath, sidecarScheme);
 	}));
 }
 
 export function deactivate() { }
 
 class MemFSWebviewPanel {
-	public static readonly viewType = 'memFSWebview';
+	public static readonly viewType = 'SideCarWebview';
 
-	public static show(extensionUri: string) {
+	public static show(extensionUri: string, sidecarScheme: string) {
 		const column = vscode.window.activeTextEditor
 			? vscode.window.activeTextEditor.viewColumn
 			: undefined;
 
-		const resourceRoots = [vscode.Uri.parse(`${scheme}`), vscode.Uri.parse(`${extensionUri}/media`)];
+		const resourceRoots = [vscode.Uri.parse(`${sidecarScheme}:${extensionUri}/media`)];
 
 		const panel = vscode.window.createWebviewPanel(
 			MemFSWebviewPanel.viewType,
-			"MemFS Webview",
+			"SideCar Webview",
 			column || vscode.ViewColumn.One,
 			{
 				localResourceRoots: resourceRoots
 			}
 		);
 
-		panel.webview.html = this._getHtmlForWebview(panel.webview, extensionUri);
+		panel.webview.html = this._getHtmlForWebview(panel.webview, extensionUri, sidecarScheme);
 	}
 
-	private static _getHtmlForWebview(webview: vscode.Webview, extensionUri: string) {
+	private static _getHtmlForWebview(webview: vscode.Webview, extensionUri: string, sidecarScheme: string) {
 		// Get resource paths
-		const styleUri = webview.asWebviewUri(vscode.Uri.parse(`${scheme}styles.css`));
+		const styleUri = webview.asWebviewUri(vscode.Uri.parse(`${sidecarScheme}:${extensionUri}/media/styles.css`));
 		// const styleUri = '';
-		const codiconsUri = webview.asWebviewUri(vscode.Uri.parse(`${extensionUri}/media/codicon.css`));
-		const codiconsFontUri = webview.asWebviewUri(vscode.Uri.parse(`${extensionUri}/media/codicon.ttf`));
+		const codiconsUri = webview.asWebviewUri(vscode.Uri.parse(`${sidecarScheme}:${extensionUri}/mediacodicon.css`));
+		const codiconsFontUri = webview.asWebviewUri(vscode.Uri.parse(`${sidecarScheme}:${extensionUri}/mediacodicon.ttf`));
 
 		return `<!DOCTYPE html>
 			<html lang="en">
@@ -396,285 +395,3 @@ class MemFSWebviewPanel {
 			</html>`;
 	}
 }
-
-export class File implements vscode.FileStat {
-
-	type: vscode.FileType;
-	ctime: number;
-	mtime: number;
-	size: number;
-
-	name: string;
-	data?: Uint8Array;
-
-	constructor(name: string) {
-		this.type = vscode.FileType.File;
-		this.ctime = Date.now();
-		this.mtime = Date.now();
-		this.size = 0;
-		this.name = name;
-	}
-}
-
-export class Directory implements vscode.FileStat {
-
-	type: vscode.FileType;
-	ctime: number;
-	mtime: number;
-	size: number;
-
-	name: string;
-	entries: Map<string, File | Directory>;
-
-	constructor(name: string) {
-		this.type = vscode.FileType.Directory;
-		this.ctime = Date.now();
-		this.mtime = Date.now();
-		this.size = 0;
-		this.name = name;
-		this.entries = new Map();
-	}
-}
-
-export type Entry = File | Directory;
-
-export class MemFS implements vscode.FileSystemProvider {
-
-	root = new Directory('');
-
-	// --- manage file metadata
-
-	stat(uri: vscode.Uri): vscode.FileStat {
-		return this._lookup(uri, false);
-	}
-
-	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] {
-		const entry = this._lookupAsDirectory(uri, false);
-		const result: [string, vscode.FileType][] = [];
-		for (const [name, child] of entry.entries) {
-			result.push([name, child.type]);
-		}
-		return result;
-	}
-
-	// --- manage file contents
-
-	readFile(uri: vscode.Uri): Uint8Array {
-		const data = this._lookupAsFile(uri, false).data;
-		if (data) {
-			return data;
-		}
-		throw vscode.FileSystemError.FileNotFound();
-	}
-
-	writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean, overwrite: boolean }): void {
-		const basename = path.posix.basename(uri.path);
-		const parent = this._lookupParentDirectory(uri);
-		let entry = parent.entries.get(basename);
-		if (entry instanceof Directory) {
-			throw vscode.FileSystemError.FileIsADirectory(uri);
-		}
-		if (!entry && !options.create) {
-			throw vscode.FileSystemError.FileNotFound(uri);
-		}
-		if (entry && options.create && !options.overwrite) {
-			throw vscode.FileSystemError.FileExists(uri);
-		}
-		if (!entry) {
-			entry = new File(basename);
-			parent.entries.set(basename, entry);
-			this._fireSoon({ type: vscode.FileChangeType.Created, uri });
-		}
-		entry.mtime = Date.now();
-		entry.size = content.byteLength;
-		entry.data = content;
-
-		this._fireSoon({ type: vscode.FileChangeType.Changed, uri });
-	}
-
-	// --- manage files/folders
-
-	rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): void {
-
-		if (!options.overwrite && this._lookup(newUri, true)) {
-			throw vscode.FileSystemError.FileExists(newUri);
-		}
-
-		const entry = this._lookup(oldUri, false);
-		const oldParent = this._lookupParentDirectory(oldUri);
-
-		const newParent = this._lookupParentDirectory(newUri);
-		const newName = path.posix.basename(newUri.path);
-
-		oldParent.entries.delete(entry.name);
-		entry.name = newName;
-		newParent.entries.set(newName, entry);
-
-		this._fireSoon(
-			{ type: vscode.FileChangeType.Deleted, uri: oldUri },
-			{ type: vscode.FileChangeType.Created, uri: newUri }
-		);
-	}
-
-	delete(uri: vscode.Uri): void {
-		const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-		const basename = path.posix.basename(uri.path);
-		const parent = this._lookupAsDirectory(dirname, false);
-		if (!parent.entries.has(basename)) {
-			throw vscode.FileSystemError.FileNotFound(uri);
-		}
-		parent.entries.delete(basename);
-		parent.mtime = Date.now();
-		parent.size -= 1;
-		this._fireSoon({ type: vscode.FileChangeType.Changed, uri: dirname }, { uri, type: vscode.FileChangeType.Deleted });
-	}
-
-	createDirectory(uri: vscode.Uri): void {
-		const basename = path.posix.basename(uri.path);
-		const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-		const parent = this._lookupAsDirectory(dirname, false);
-
-		const entry = new Directory(basename);
-		parent.entries.set(entry.name, entry);
-		parent.mtime = Date.now();
-		parent.size += 1;
-		this._fireSoon({ type: vscode.FileChangeType.Changed, uri: dirname }, { type: vscode.FileChangeType.Created, uri });
-	}
-
-	// --- lookup
-
-	private _lookup(uri: vscode.Uri, silent: false): Entry;
-	private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined;
-	private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined {
-		const parts = uri.path.split('/');
-		let entry: Entry = this.root;
-		for (const part of parts) {
-			if (!part) {
-				continue;
-			}
-			let child: Entry | undefined;
-			if (entry instanceof Directory) {
-				child = entry.entries.get(part);
-			}
-			if (!child) {
-				if (!silent) {
-					throw vscode.FileSystemError.FileNotFound(uri);
-				} else {
-					return undefined;
-				}
-			}
-			entry = child;
-		}
-		return entry;
-	}
-
-	private _lookupAsDirectory(uri: vscode.Uri, silent: boolean): Directory {
-		const entry = this._lookup(uri, silent);
-		if (entry instanceof Directory) {
-			return entry;
-		}
-		throw vscode.FileSystemError.FileNotADirectory(uri);
-	}
-
-	private _lookupAsFile(uri: vscode.Uri, silent: boolean): File {
-		const entry = this._lookup(uri, silent);
-		if (entry instanceof File) {
-			return entry;
-		}
-		throw vscode.FileSystemError.FileIsADirectory(uri);
-	}
-
-	private _lookupParentDirectory(uri: vscode.Uri): Directory {
-		const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-		return this._lookupAsDirectory(dirname, false);
-	}
-
-	// --- manage file events
-
-	private _emitter = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
-	private _bufferedEvents: vscode.FileChangeEvent[] = [];
-	private _fireSoonHandle?: NodeJS.Timer;
-
-	readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this._emitter.event;
-
-	watch(_resource: vscode.Uri): vscode.Disposable {
-		// ignore, fires for all changes...
-		return new vscode.Disposable(() => { });
-	}
-
-	private _fireSoon(...events: vscode.FileChangeEvent[]): void {
-		this._bufferedEvents.push(...events);
-
-		if (this._fireSoonHandle) {
-			clearTimeout(this._fireSoonHandle);
-		}
-
-		this._fireSoonHandle = setTimeout(() => {
-			this._emitter.fire(this._bufferedEvents);
-			this._bufferedEvents.length = 0;
-		}, 5);
-	}
-}
-
-export const styleContent = `
-html {
-	box-sizing: border-box;
-	font-size: 16px;
-}
-
-*, *:before, *:after {
-	box-sizing: inherit;
-}
-
-body, h1, h2, h3, h4, h5, h6, p, ol, ul {
-	margin: 0;
-	padding: 0;
-	font-weight: normal;
-}
-
-ol, ul {
-	list-style: none;
-}
-
-img {
-	max-width: 100%;
-	height: auto;
-}
-
-/* Demo Styles */
-
-body {
-	font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
-	margin: 0;
-	padding: 10px 20px;
-	text-align: center;
-	color: var(--vscode-foreground);
-	background-color: var(--vscode-editor-background);
-}
-
-h1 {
-	font-weight: bold;
-	margin: 24px;
-}
-
-#icons {
-	display: flex;
-	flex-wrap: wrap;
-	align-items: center;
-    justify-content: center;
-}
-
-#icons .icon {
-	width: 140px;
-	padding: 20px;
-	font-size: 14px;
-	display: flex;
-	flex-direction: column;
-	text-align: center;
-}
-
-#icons .icon .codicon {
-	font-size: 32px;
-	padding-bottom: 16px;
-}
-`;
